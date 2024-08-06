2b27e263f783b9787820dff8bec5114ab8cb613e
Rashmi Ranjan Bisoyi <135081120+rashmi-ranjan07@users.noreply.github.com>
2024-08-06 20:46:10 +0530

    Add files via upload

---
+++ users_area/checkout.php
@@ -0,0 +1,149 @@
+<!--Connect FIle-->
+<?php
+include('../includes/connect.php');
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" contents="IE=edge">
+    <meta name="viewreport" contents="width=device-width, initial-scale=1.0">
+    <title>Wings Cart-Checkout page</title>
+    <!-- bootstrap CSS LINK -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+    crossorigin="anonymous">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- css file-->
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .logo{
+            width:5%;
+            height:5%;
+        }
+        .card-body {
+   width: 100%;
+   height: 250px;
+   object-fit: contain;
+}
+        </style>
+   </head> 
+   <body>
+<!-- NAVBAR-->
+<div class="container-fluid p-0">
+    <!-- first child-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <div class="container-fluid">
+  <img src="../images/Logo2.png" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+    aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../display_all.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="user_registration.php">Register</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+        </ul>
+        <form class= "d-flex" action="search_product.php" method="get">
+          <input class="form-control me-2" type="search" placeholder="Search" 
+          aria-label="Search" name="search_data">
+          <input type="submit" value="Search" class="btn btn-outline-light" 
+          name="search_data_product">
+        </form>
+        </div>
+        </div>
+        </nav>
+        </div>
+
+        
+
+<!-- second child-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <ul class="navbar-nav me-auto">
+ 
+        <?php
+        if(!isset($_SESSION['username'])){
+          echo" <li class='nav-item'>
+          <a class='nav-link' href='#'>Welcome Guest</a>
+        </li>";
+         }else{
+          echo"<li class='nav-item'>
+          <a class='nav-link' href='#'>Welcome ".$_SESSION['username']."</a>
+        </li>";
+         }
+       if(!isset($_SESSION['username'])){
+        echo" <li class='nav-item'>
+        <a class='nav-link' href='./user_login.php'>Login</a>
+      </li>";
+       }else{
+        echo" <li class='nav-item'>
+        <a class='nav-link' href='logout.php'>Logout</a>
+      </li>";
+
+       }
+        ?>
+        
+  </ul>
+</nav>
+
+
+
+<!--third child-->
+<div class="bg-light">
+   <h3 class="text-center">Hidden Store</h3>
+   <p class="text-center">Communications is at the heart of e-commerce
+   and community</p>
+</div>
+
+
+
+
+<!--fourth child-->
+<div class="row p-1">
+  <div class="col-md-10">
+    <!-- products -->
+      <div class="row">
+        <?php
+        if(!isset($_SESSION['username'])){
+            include('user_login.php');
+        }else{
+            include('payment.php');
+        }
+
+        ?>
+       
+<!--row end-->
+</div>
+<!--column end-->
+</div>
+</div>
+<!-- last child-->
+<?php include("../includes/footer.php")?>
+
+
+
+
+<!--Bootstrap JS link-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" 
+crossorigin="anonymous"></script>
+   </body>
+</html>
---
+++ users_area/confirm_payment.php
@@ -0,0 +1,74 @@
+<?php
+include('../includes/connect.php');
+session_start();
+if(isset($_GET['order_id'])){
+    $order_id=$_GET['order_id'];
+    //echo  $order_id;
+
+    $select_data="SELECT * FROM `user_orders` WHERE order_id=$order_id";
+    $result=mysqli_query($con,$select_data);
+    $row_fetch=mysqli_fetch_assoc($result);
+    $invoice_number=$row_fetch['invoice_number'];
+    $amount_due=$row_fetch['amount_due'];
+
+}
+
+if(isset($_POST['confirm_payment'])){
+    $invoice_number=$_POST['invoice_number'];
+    $amount=$_POST['amount'];
+    $payment_mode=$_POST['payment_mode'];
+    $insert_query="INSERT INTO `user_payments` (order_id,invoice_number,amount,payment_mode)
+    VALUES ($order_id,$invoice_number,$amount,'$payment_mode')";
+    $result=mysqli_query($con,$insert_query);
+    if($result){
+        echo "<h3 class='text-center text-light'>Successfully completed the payment</h3>";
+        echo "<script>window.open('profile.php?my_orders','_self')</script>";
+    }  
+    $update_orders="UPDATE `user_orders` set order_status='Complete' where order_id=$order_id";
+    $result_orders=mysqli_query($con,$update_orders);
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment page</title>
+    <!-- bootstrap CSS LINK -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+    crossorigin="anonymous">
+</head>
+<body class="bg-secondary">
+    
+    <div class="container my-5"> 
+    <h1 class="text-center text-light">Confirm Payment</h1>
+        <form action="" method="post">
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <input type="text" class="form-control w-50 m-auto" name="invoice_number" value="<?php echo $invoice_number ?>">
+            </div>
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <label for="" class="text-light">Amount</label>
+                <input type="text" class="form-control w-50 m-auto" name="amount" value="<?php echo $amount_due ?>">
+            </div>
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <select name="payment_mode" class="form-select w-50 m-auto">
+                    <option>Select Payment Mode</option>
+                    <option>UPI</option>
+                    <option>Netbanking</option>
+                    <option>Paypal</option>
+                    <option>Cash On Delivery</option>
+                    <option>Payoffline</option>
+                </select>
+            </div>
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <input type="submit" class="bg-info py-2 px-3 border-0" value="Confirm" name="confirm_payment">
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
